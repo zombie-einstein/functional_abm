dc9fda583d6249eecf29e41141d742a6c6bf6bdf
zombie-einstein <>
2020-06-22 00:20:05 +0100

    Add simple example

---
+++ examples/simple_example.py
@@ -0,0 +1,31 @@
+from functional_abm.agent import agent
+from functional_abm.schedulers.step_based import StepBasedScheduler
+
+
+class AgentState:
+    def __init__(self, name: str):
+        self.name = name
+        self.value = 0
+
+
+def model():
+
+    scheduler = StepBasedScheduler(20)
+
+    @agent(scheduler=scheduler)
+    def simple_agent(t, antecedents, state, descendants):
+        print(f"Agent: {state.name}, Value: {state.value}, t: {t}")
+        state.value += 1
+        return state, descendants, t + 2
+
+    agent_state_1 = AgentState("1")
+    agent_state_2 = AgentState("2")
+
+    agent_1 = simple_agent(0, [], agent_state_1, {})
+    agent_2 = simple_agent(1, [], agent_state_2, {})
+
+    while not scheduler.finished:
+        scheduler.step()
+
+
+model()
